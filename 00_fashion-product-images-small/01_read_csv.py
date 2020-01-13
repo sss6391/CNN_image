4d91776e1d10e37c5e605633a1e2134cd24ec218
sss6391 <50129532+sss6391@users.noreply.github.com>
2020-01-14 07:36:24 +0900

    save csv

--- 00_fashion-product-images-small/01_read_csv.py
+++ 00_fashion-product-images-small/01_read_csv.py
@@ -2,21 +2,33 @@ import pandas as pd
 import cv2
 import numpy as np
 
-# csv파일 읽기 및 에러라인 읽기 제외
+# csv파일 읽기 및 에러라인 읽기 제외 (error_bad_lines=False)
 data_frame = pd.read_csv("styles.csv", error_bad_lines=False)
 # id 오름차순 정렬
 sorted_data = data_frame.sort_values(by=["id"])
-# 옷이 아닌 데이터 제외
+# 옷이 아닌 데이터 제외 선택
 selected_data = sorted_data[sorted_data["masterCategory"].isin(["Apparel"]) &
                             sorted_data["subCategory"].isin(["Topwear", "Bottomwear"])]
 
 # 카테고리 종류 확인
-drop_data = selected_data.drop_duplicates("articleType")
-print(drop_data["articleType"])
+# drop_data = selected_data.drop_duplicates("articleType")
+# print(drop_data["articleType"])
+
+# 딥러닝용 라벨링 추가
+selected_data.loc[ selected_data["subCategory"].str.contains("Topwear"), "1st_label" ] = 0
+selected_data.loc[ selected_data["subCategory"].str.contains("Bottomwear"), "1st_label" ] = 1
+
+# 라벨링 확인
+check_data = selected_data[selected_data["1st_label"].isin([1]) ]
+print(check_data["subCategory"])
+
+selected_data.to_csv("selected_data.csv", index=False)
+
 
 '''
+# 선택된 데이터 id 읽기
 id_data = selected_data["id"]
-# 이미지 읽어 배열로 변환
+# id로 해당하는 이미지파일 배열로 변환
 images = []
 for ids in id_data:
     image = cv2.imread('images/' + str(ids))
